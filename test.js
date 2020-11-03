c503b60fbd69b341afba0c8df92622835e8482a6
ashersaupingomez <ashersaupingomez@gmail.com>
2020-11-03 16:28:36 +1100

    Added: testing & documentation

--- test.js
+++ test.js
@@ -1,26 +1,15 @@
 const baretest = require('baretest');
 const assert = require('assert');
 
-const { createClient, useClient } = require('./lib');
+const { createClient, useClient, getAPList } = require('./lib');
 const { name } = require('./package');
 
 const test = baretest(name);
 
-function getUserInfo(client) {
-  return client
-    .get('/user_info.xml');
-}
+test('getAPList works', async () => {
+  const devices = await useClient(createClient(), getAPList);
 
-const client = createClient();
-
-test('login, execute a function, then logout', async () => {
-  const { ok } = await useClient(client, getUserInfo);
-
-  assert.ok(ok);
-});
-
-test('fails to execute a function after logout', () => {
-  assert.rejects(() => getUserInfo(client));
+  assert(Array.isArray(devices));
 });
 
 test.run();
